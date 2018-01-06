a69135a5508a4b6f3c690513607a29868c2679ea
朱剑豪 <zhujianhao@zhujianhaodeMacBook-Pro.local>
2018-01-07 02:14:10 +0800

    listen for more mouse events

--- fiengine/component/FI_Touchable.js
+++ fiengine/component/FI_Touchable.js
@@ -5,11 +5,11 @@ export default class FI_Touchable extends FI_Component{
     this.onClick = func
   }
   onMount(){
-    MouseCenter.getInstance().addListener('mousedown', this.onMouseDown.bind(this), this.node.getLevel() )
-    MouseCenter.getInstance().addListener('mouseup', this.onMouseUp.bind(this), this.node.getLevel() )
+    MouseCenter.getInstance().addListener('mousedown', this._onMouseDown.bind(this), this.node.getLevel() )
+    MouseCenter.getInstance().addListener('mouseup', this._onMouseUp.bind(this), this.node.getLevel() )
+    MouseCenter.getInstance().addListener('mousemove', this._onMouseMove.bind(this), this.node.getLevel())
   }
-  onMouseDown(e){
-
+  _onMouseMove(e){
     var anchorOffset = this.node.getAnchorOffset()
     var l = this.transform.e - anchorOffset.x
     var r = l + this.node.size.width
@@ -21,30 +21,18 @@ export default class FI_Touchable extends FI_Component{
       e.clientY >= t &&
       e.clientY <= b
     ){
-      this.isMouseDownOnMe = true
-      console.log(this.transform)
-      console.log(this.transform.inverse())
-      return true
+      !this.isMouseOnMe && this.onMouseOut && this.onMouseIn(this)
+      this.isMouseOnMe = true
+    }else{
+      this.isMouseOnMe && this.onMouseOut && this.onMouseOut(this)
+      this.isMouseOnMe = false
     }
   }
-  onMouseUp(e){
-    if(!this.isMouseDownOnMe){
-      return
-    }
-    this.isMouseDownOnMe = false
-    var anchorOffset = this.node.getAnchorOffset()
-    var l = this.transform.e - anchorOffset.x
-    var r = l + this.node.size.width
-    var t = this.transform.f - anchorOffset.y
-    var b = t + this.node.size.height
-    if(
-      e.clientX >= l &&
-      e.clientX <= r &&
-      e.clientY >= t &&
-      e.clientY <= b
-    ){
-      this.onClick(this)
-    }
+  _onMouseDown(e){
+    this.isMouseOnMe && this.onMouseDown && this.onMouseDown(this)
+  }
+  _onMouseUp(e){
+    this.isMouseOnMe && this.onMouseUp && this.onMouseUp(this)
   }
   draw(ctx){
     this.debugDraw(ctx)
