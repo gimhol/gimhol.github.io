62e8fa7fc8a0453b696ffde3e719a242a893f800
朱剑豪 <zhujianhao@zhujianhaodeMacBook-Pro.local>
2018-01-08 21:21:43 +0800

    use action 'ScaleTo'

--- app/ui/Button.js
+++ app/ui/Button.js
@@ -1,14 +1,15 @@
 import FI_Node from '../../fiengine/node/FI_Node'
 import FI_Text from '../../fiengine/component/FI_Text'
 import FI_Touchable from '../../fiengine/component/FI_Touchable'
-
+import { FI_ScaleTo } from '../../fiengine/action/FI_Scale'
 export default class Button extends FI_Node{
   constructor(textContent){
     super()
     this.textContent = textContent
   }
   onAdded(){
-    var textNode = this.addChild(new FI_Node())
+    var inner = this.addChild(new FI_Node())
+    var textNode = inner.addChild(new FI_Node())
 
     var textComponent = textNode.addComponent(new FI_Text())
     textComponent.content = this.textContent
@@ -18,20 +19,20 @@ export default class Button extends FI_Node{
 
     var touchableComponent = this.addComponent(new FI_Touchable())
     touchableComponent.onMouseIn = (t)=>{
-      t.node.scale.x = 1.1
-      t.node.scale.y = 1.1
+      inner.removeAllActions()
+      inner.addAction(new FI_ScaleTo(1.1,1.1,250))
     }
     touchableComponent.onMouseOut = (t)=>{
-      t.node.scale.x = 1.0
-      t.node.scale.y = 1.0
+      inner.removeAllActions()
+      inner.addAction(new FI_ScaleTo(1,1,250))
     }
     touchableComponent.onMouseDown = (t)=>{
-      t.node.scale.x = 1.0
-      t.node.scale.y = 1.0
+      inner.removeAllActions()
+      inner.addAction(new FI_ScaleTo(1,1,75))
     }
     touchableComponent.onMouseUp = (t)=>{
-      t.node.scale.x = 1.1
-      t.node.scale.y = 1.1
+      inner.removeAllActions()
+      inner.addAction(new FI_ScaleTo(1.1,1.1,75))
     }
   }
 }
